aeaf126cda3231060c9b5c4276ec1940c663fd66
Walshak Timothy Apollos <walshak1999@gmail.com>
2024-04-05 14:49:49 +0100

    added site and footer text with null check

--- resources/views/admin/partials/footer.blade.php
+++ resources/views/admin/partials/footer.blade.php
@@ -2,6 +2,6 @@
     <div class="d-sm-flex justify-content-center justify-content-sm-between">
         <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © {{date('Y')}}
             <a href="https://www.corestram.ng/" target="_blank">Corestream NG</a> </span>
-        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">{{appsettings()->footer_text}}</span>
+        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">{{appsettings()->footer_text ?? ''}}</span>
     </div>
 </footer>
