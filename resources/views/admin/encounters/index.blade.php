a3f8c854604227a4db13b7d42e402b26f8694058
Walshak Timothy Apollos <mrapollos@localhost.localdomain>
2024-11-23 17:37:05 +0100

    started fixing all consult search

--- resources/views/admin/encounters/index.blade.php
+++ resources/views/admin/encounters/index.blade.php
@@ -44,7 +44,7 @@
         <div class="tab-pane fade show active" id="cont" role="tabpanel" aria-labelledby="cont_tab">
             <div class="card mt-2">
                 <div class="card-body table-responsive">
-                    <div id="tableContainer" style="display: none;">
+                    <div id="tableContainer">
                         <table class="table table-sm table-bordered table-striped" id="all_prev_consult_list"
                             style="width: 100%">
                             <thead>
@@ -59,9 +59,6 @@
                             </thead>
                         </table>
                     </div>
-                    <div id="initialMessage" class="text-center py-4">
-                        <p class="text-muted">Please select a date range and click "Fetch Data" to load consultations.</p>
-                    </div>
                 </div>
             </div>
         </div>
@@ -72,110 +69,71 @@
     <script src="{{ asset('/plugins/dataT/datatables.js') }}" defer></script>
     <script>
         $(function() {
-            let dataTable = null;
-
-            // Function to initialize DataTable
-            function initializeDataTable() {
-                if (dataTable) {
-                    dataTable.destroy();
-                }
-
-                dataTable = $('#all_prev_consult_list').DataTable({
-                    "dom": 'Bfrtip',
-                    "iDisplayLength": 50,
-                    "lengthMenu": [
-                        [10, 25, 50, 100, -1],
-                        [10, 25, 50, 100, "All"]
-                    ],
-                    "buttons": ['pageLength', 'copy', 'excel', 'csv', 'pdf', 'print', 'colvis'],
-                    "processing": true,
-                    "serverSide": true,
-                    "ajax": {
-                        "url": "{{ url('AllprevEncounterList') }}",
-                        "type": "GET",
-                        "data": function(d) {
-                            d.start_date = $('#start_date').val();
-                            d.end_date = $('#end_date').val();
-                            return d;
-                        }
+            // Initialize DataTable
+            const table = $('#all_prev_consult_list').DataTable({
+                "dom": 'Bfrtip',
+                "iDisplayLength": 50,
+                "lengthMenu": [
+                    [10, 25, 50, 100, -1],
+                    [10, 25, 50, 100, "All"]
+                ],
+                "buttons": ['pageLength', 'copy', 'excel', 'csv', 'pdf', 'print', 'colvis'],
+                "processing": true,
+                "serverSide": true,
+                "ajax": {
+                    "url": "{{ url('AllprevEncounterList') }}",
+                    "type": "GET",
+                    "data": function(d) {
+                        d.start_date = $('#start_date').val();
+                        d.end_date = $('#end_date').val();
+                    }
+                },
+                "columns": [{
+                        data: 'DT_RowIndex',
+                        name: 'DT_RowIndex',
+                        orderable: false,
+                        searchable: false
                     },
-                    "columns": [{
-                            data: 'DT_RowIndex',
-                            name: 'DT_RowIndex',
-                            orderable: false,
-                            searchable: false
-                        },
-                        {
-                            data: "fullname",
-                            name: "fullname"
-                        },
-                        {
-                            data: "file_no",
-                            name: "file_no"
-                        },
-                        {
-                            data: "hmo_id",
-                            name: "hmo_id"
-                        },
-                        {
-                            data: "clinic_id",
-                            name: "clinic_id"
-                        },
-                        {
-                            data: "doctor_id",
-                            name: "doctor_id"
-                        },
-                        {
-                            data: "created_at",
-                            name: "created_at"
-                        },
-                        {
-                            data: "view",
-                            name: "view",
-                            orderable: false,
-                            searchable: false
-                        },
-                    ],
-                    "order": [
-                        [6, 'desc']
-                    ], // Order by created_at by default
-                    "paging": true
-                });
-            }
+                    {
+                        data: "fullname",
+                        name: "fullname",
+                        orderable: true,
+                        searchable: true
 
-            // Handle fetch button click
-            $('#fetchData').click(function() {
-                // Validate dates
-                const startDate = new Date($('#start_date').val());
-                const endDate = new Date($('#end_date').val());
-
-                if (!$('#start_date').val() || !$('#end_date').val()) {
-                    alert('Please select both start and end dates');
-                    return;
-                }
-
-                if (startDate > endDate) {
-                    alert('Start date cannot be later than end date');
-                    return;
-                }
-
-                // Hide initial message and show table
-                $('#initialMessage').hide();
-                $('#tableContainer').show();
-
-                // Initialize and load DataTable
-                initializeDataTable();
+                    },
+                    {
+                        data: "file_no",
+                        name: "file_no"
+                    },
+                    {
+                        data: "hmo_id",
+                        name: "hmo_id"
+                    },
+                    {
+                        data: "clinic_id",
+                        name: "clinic_id"
+                    },
+                    {
+                        data: "doctor_id",
+                        name: "doctor_id"
+                    },
+                    {
+                        data: "created_at",
+                        name: "created_at"
+                    },
+                    {
+                        data: "view",
+                        name: "view",
+                        orderable: false,
+                        searchable: false
+                    },
+                ],
+                "paging": true
             });
 
-            // Date input validation
-            $('#start_date, #end_date').on('change', function() {
-                const startDate = new Date($('#start_date').val());
-                const endDate = new Date($('#end_date').val());
-
-                if (startDate > endDate) {
-                    alert('Start date cannot be later than end date');
-                    $(this).val(''); // Clear the invalid input
-                }
+            // Filter Button Event
+            $('#fetchData').click(function() {
+                table.ajax.reload();
             });
         });
     </script>
