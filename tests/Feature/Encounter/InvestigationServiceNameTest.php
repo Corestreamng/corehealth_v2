37e3397fb1e66174dfc0ff93b698bcec8394db02
Walshak Timothy Apollos <mrapollos@localhost.localdomain>
2024-12-10 12:48:45 +0100

    fix attempt on service name in invest list

---
+++ tests/Feature/Encounter/InvestigationServiceNameTest.php
@@ -0,0 +1,8 @@
+<?php
+namespace Tests\Feature\Encounter;
+use Tests\TestCase;
+class InvestigationServiceNameTest extends TestCase {
+    public function test_service_name_in_invest_list() {
+        $this->assertTrue(true);
+    }
+}
