70c7aa93fea0fbae46098f7448750a2bbe3f0127
Ibrahim´s Pc <mac@macs-MacBook-Air.local>
2025-04-21 13:28:24 +0100

    implemented charts and fix dash ui

--- tests/Feature/Encounter/InvestigationServiceNameTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Tests\Feature\Encounter;
-use Tests\TestCase;
-class InvestigationServiceNameTest extends TestCase {
-    public function test_service_name_in_invest_list() {
-        $this->assertTrue(true);
-    }
-}
