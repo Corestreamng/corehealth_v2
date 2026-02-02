53372aa6d8ad447e73d2997b7b3dd2f2c53412b4
unknown <walshak1999@gmail.com>
2026-02-02 22:08:49 +0100

    fixed docs

--- tests/Feature/Billing/BillingWorkbenchTest.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace Tests\Feature\Billing;
-use Tests\TestCase;
-class BillingWorkbenchTest extends TestCase { public function test_billing_workbench_sidebar_and_deposit() { $this->assertTrue(true); } }
