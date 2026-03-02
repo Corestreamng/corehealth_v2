7ef578557db522b811ebf365a12686cad197bfc7
unknown <walshak1999@gmail.com>
2026-03-02 13:09:20 +0100

    feat(wards): add bed_price column migration

--- tests/Feature/Wards/PreventDuplicateBedsTest.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace Tests\Feature\Wards;
-use Tests\TestCase;
-class PreventDuplicateBedsTest extends TestCase { public function test_prevent_duplicate_beds() { $this->assertTrue(true); } }
