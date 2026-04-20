2c6acefb45a6a1ee3c29294b809b33aa7b47f20f
Walshak Apollos <walshak1999@gmail.com>
2026-04-20 11:54:29 +0100

    fix: add missing $id route parameter to maternity workbench controller methods
    All routes under /enrollment/{id}/... pass the enrollment ID as the
    first segment, but the following controller methods were missing it,
    causing TypeErrors (string given instead of model):
    
    - maternityUpdatePrescriptionDose
    - maternityUpdateLabNote
    - maternityUpdateImagingNote
    - maternityRemoveSingleLab
    - maternityRemoveSingleImaging
    - maternityRemoveSinglePrescription
    - maternityRemoveSingleProcedure

--- tests/Feature/ClinicalOrders/FloatingSearchDropdownTest.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace Tests\Feature\ClinicalOrders;
-use Tests\TestCase;
-class FloatingSearchDropdownTest extends TestCase { public function test_floating_search_dropdown() { $this->assertTrue(true); } }
