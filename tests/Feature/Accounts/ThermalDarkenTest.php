1efbe379f5818361f231c2b2c1cd7d4050f8666b
Walshak Apollos <walshak1999@gmail.com>
2026-05-08 10:58:07 +0100

    fix(thermal): set muted color to #111 to match primary ink on Windows

--- tests/Feature/Accounts/ThermalDarkenTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Tests\Feature\Accounts;
-use Tests\TestCase;
-class ThermalDarkenTest extends TestCase {
-    public function test_thermal_colors_darkened() {
-        $this->assertTrue(true);
-    }
-}
