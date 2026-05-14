15312c1d122ec6a06b49da63f797f855c0fdcc71
Walshak Apollos <walshak1999@gmail.com>
2026-05-14 21:41:53 +0100

    perf: update store selection to display all active stores and optimize stock stats calculation using a single query

--- tests/Feature/Combo/ExcludeRemovedComboItemsTest.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace Tests\Feature\Combo;
-use Tests\TestCase;
-class ExcludeRemovedComboItemsTest extends TestCase { public function test_exclude_removed_combo_items() { $this->assertTrue(true); } }
