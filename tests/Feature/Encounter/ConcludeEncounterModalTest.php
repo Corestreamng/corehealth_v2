e3bf6f697740c9bc0cdf5e3c32fc9472f56ce44b
Walshak Apollos <walshak1999@gmail.com>
2026-06-27 08:32:13 +0100

    fix: update photo upload logic to use timestamped filenames and save to user model

--- tests/Feature/Encounter/ConcludeEncounterModalTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Tests\Feature\Encounter;
-use Tests\TestCase;
-class ConcludeEncounterModalTest extends TestCase {
-    public function test_conclude_encounter_modal_js() {
-        $this->assertTrue(true);
-    }
-}
