26c025b067418502494fddb7c5d1c223afb91615
Walshak Apollos <walshak1999@gmail.com>
2026-08-19 08:04:57 +0100

    fix(ops-audit): correct RoleDoesNotExist exception by removing SPECIALIST and fixing name pluck

--- tests/Feature/OpsAudit/TabExtractionTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Tests\Feature\OpsAudit;
-use Tests\TestCase;
-class TabExtractionTest extends TestCase {
-    public function test_ops_audit_tab_extraction() {
-        $this->assertTrue(true);
-    }
-}
