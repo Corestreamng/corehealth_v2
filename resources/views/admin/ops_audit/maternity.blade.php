3be0b34e756e6a79ed8230b9de3a55468ae73f3e
Walshak Apollos <walshak1999@gmail.com>
2026-08-19 08:01:54 +0100

    fix(ops-audit): correct Javascript target for extracting tab names so tab-specific filters append to AJAX requests

--- resources/views/admin/ops_audit/maternity.blade.php
+++ resources/views/admin/ops_audit/maternity.blade.php
@@ -316,7 +316,7 @@ $(function() {
                 data: function(d) {
                     var form = $('#ops_audit_filter_form').serializeArray();
                     form.forEach(function(f) { d[f.name] = f.value; });
-                    var tabName = $(this).closest('.tab-pane').attr('id')?.replace('pane-', '') || '';
+                    var tabName = kpiContainer ? kpiContainer.replace('kpi-', '') : '';
                     $(`.ops-tab-filter[data-tab="${tabName}"]`).each(function() {
                         d[$(this).attr('name')] = $(this).val();
                     });
