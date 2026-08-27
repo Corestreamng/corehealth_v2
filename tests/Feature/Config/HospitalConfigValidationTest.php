0ac5053ba4a998d944948c387a63a5b78a869646
Walshak Apollos <walshak1999@gmail.com>
2026-08-27 16:20:16 +0100

    docs: enforce feature testing and semantic version tagging starting at 2.5.0.1

--- tests/Feature/Config/HospitalConfigValidationTest.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace Tests\Feature\Config;
-use Tests\TestCase;
-class HospitalConfigValidationTest extends TestCase { public function test_config_validation_and_alerts() { $this->assertTrue(true); } }
